49d6f765414bf10aff704b40f90dc35c53525f7c
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2020-06-12 16:05:03 +0100

    Create TSC project

---
+++ src/FileBrowser.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const FileBrowser = React.memo(() => {
+    return <p>I am a file browser</p>;
+});
---
+++ src/index.ts
@@ -0,0 +1,2 @@
+export {FileBrowser} from './FileBrowser';
+
