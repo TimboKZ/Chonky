c5242f1897a85e863e0e5f28b18a24725208bddb
Tim Kuzhagaliyev <tim.kuzh@gmail.com>
2020-10-20 14:36:13 +0100

    Add shared TypeScript config, update icon package

--- packages/chonky-icon-fontawesome/lib/chonky-icon-fontawesome.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-module.exports = chonkyIconFontawesome;
-
-function chonkyIconFontawesome() {
-    // TODO
-}
---
+++ packages/chonky-icon-fontawesome/src/ChonkyIconFA.tsx
@@ -0,0 +1,12 @@
+/**
+ * @author Timur Kuzhagaliyev <tim.kuzh@gmail.com>
+ * @copyright 2020
+ * @license MIT
+ */
+
+import { ChonkyIconProps } from 'chonky/lib/components/external/ChonkyIcon';
+import React from 'react';
+
+export const ChonkyIconFA: React.FC<ChonkyIconProps> = (props) => {
+    return <h1>ChonkyIconFA component.</h1>;
+};
